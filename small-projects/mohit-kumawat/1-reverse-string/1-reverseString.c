f084213dda269fc9719134126a0703df7adaf81d
Mohit <i.kumawat.mohit@gmail.com>
2025-10-02 10:07:42 +0530

    docs: add comments to reverse string c code

--- small-projects/mohit-kumawat/1-reverse-string/1-reverseString.c
+++ small-projects/mohit-kumawat/1-reverse-string/1-reverseString.c
@@ -11,19 +11,23 @@ Date: 2025-10-02
 #include <string.h>
 
 char* reverse_str(const char *input){
-    int len = strlen(input);
+    int len = strlen(input); 
+
+    // dynamically allocated memory for pointer "output"
     char *output = malloc(len +1);
 
+    // standard malloc procedure
     if(output == NULL){
         printf("Memory allocation failed.\n");
         return NULL;
     }
 
+    // reverse string logic
     for (int i=0; i<len; i++){
         output[i] = input[len - 1 -i];
     }
 
-    output[len] = '\0';
+    output[len] = '\0'; // nulish terminator to end the string
     return output;
 }
 
@@ -32,18 +36,21 @@ int main(void){
 
     char *reversed;
 
+    //Example: 1
     reversed = reverse_str("Hello world");
     if(reversed != NULL){
         printf("%s\n", reversed);
         free(reversed);
     }
     
+    //Example: 2
     reversed = reverse_str("Coffee");
     if (reversed != NULL) {
         printf("%s\n", reversed);
         free(reversed);
     }
 
+    //Example: 3
     reversed = reverse_str("stressed");
     if (reversed != NULL) {
         printf("%s\n", reversed);
